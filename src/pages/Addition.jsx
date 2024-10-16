b1ecba2d2be8108f5d1cb9d3da7411fc0e92bb1b
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-10-16 01:39:43 +0100

    logic

--- src/pages/Addition.jsx
+++ src/pages/Addition.jsx
@@ -5,19 +5,33 @@ import { useNavigate } from 'react-router-dom';
 export const Addition = () => {
    const navigate = useNavigate()
     const [currentQuestion, setCurrentQuestion] = useState(0)
-
+    const [selectanswer, setselectedanswer] = useState(null)
+    const [isCorrect, setIsCorrect] = useState(null);
     const questions = Object.values(AdditionQuestion)
-
+    const HandleAnswer = (answer) => {
+        setselectedanswer(answer)
+        const correctanswer = questions[currentQuestion].answer
+        if (answer === correctanswer){
+            setIsCorrect(true)
+        }else{
+            setIsCorrect(false)
+        }
+    }
     const NextQuestion = () => {
-        if (currentQuestion < questions.length -1){
+        if (selectanswer !== null && currentQuestion < questions.length -1 ){
             setCurrentQuestion(
                 currentQuestion + 1
             )
-        }else{
-            navigate('/score')
-        }
+            setselectedanswer(null)
+        }else
+            if(currentQuestion >= questions.length -1){
+                navigate('/score')
+            }
+         
+        
      
     }
+   
     return(
         <div className="h-auto bg-[#BF5700]">
             <div className="container mx-auto p-12">
@@ -37,10 +51,10 @@ export const Addition = () => {
                        <h2 className='text-8xl tracking-tight mb-5'>
                         {problem.question}
                        </h2>
-                       <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-12 py-5 mb-4'>
+                       <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-12 py-5 mb-4' >
                         {problem.options.map((Option,optionindex) => (
  <div className='flex justify-center items-center' key={optionindex}>
- <div className='flex justify-center items-center rounded-full bg-gray-700 w-24 h-24 text-4xl text-white'>
+ <div className='flex justify-center items-center rounded-full bg-gray-700 w-24 h-24 text-4xl text-white cursor-pointer'onClick={()=> HandleAnswer(Option)} >
      {Option}
  </div>
 </div>
@@ -49,15 +63,20 @@ export const Addition = () => {
 
 
                        </div>
+                 
                        
-                       <div className='bg-gray-700 py-2 px-2 mt-3 text-white'
+                       <button className={`bg-gray-700 py-2 px-2 mt-3 text-white cursor-pointer 
+                         ${selectanswer === null ? 'cursor-not-allowed':''}
+                       `}
                        
                        onClick={NextQuestion}
                        aria-disabled={currentQuestion >= questions.length-1}
                        disabled={currentQuestion >= questions.length - 1}
                        >
                                 Next Question
-                       </div>
+                       </button>
+                    
+                     
                     </div>
                 ))}
 
