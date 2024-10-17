f7cb088513780fb8621fc329b1db6247078ae450
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-10-17 08:52:23 +0100

    check

--- src/pages/Addition.jsx
+++ src/pages/Addition.jsx
@@ -1,4 +1,4 @@
-// import { CloseCircle } from 'iconsax-react';
+import { CloseCircle } from 'iconsax-react';
 import { AdditionQuestion } from '../components/Addarray';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
