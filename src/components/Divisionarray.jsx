1ead057a84d962b67c823a73717f8b48ff2f563a
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-10-20 22:57:42 +0100

    1: "deduced a function that will get the random questions to display in each category "

--- src/components/Divisionarray.jsx
+++ src/components/Divisionarray.jsx
@@ -23,5 +23,85 @@ export const DivisionQuestion = {
       question: "625/25 = ?",
       options: [10, 25, 12, 13],
       answer: 25
-    }
+    },
+    question6: {
+    question: "36/6 = ?",
+    options: [5, 6, 7, 8],
+    answer: 6
+  },
+question7: {
+    question: "81/9 = ?",
+    options: [7, 8, 9, 10],
+    answer: 9
+  },
+question8: {
+    question: "100/4 = ?",
+    options: [20, 25, 15, 30],
+    answer: 25
+  },
+question9: {
+    question: "49/7 = ?",
+    options: [6, 7, 8, 9],
+    answer: 7
+  },
+question10: {
+    question: "72/8 = ?",
+    options: [6, 7, 8, 9],
+    answer: 9
+  },
+question11: {
+    question: "120/5 = ?",
+    options: [20, 22, 23, 24],
+    answer: 24
+  },
+question12: {
+    question: "144/12 = ?",
+    options: [10, 11, 12, 13],
+    answer: 12
+  },
+question13: {
+    question: "56/7 = ?",
+    options: [7, 8, 9, 10],
+    answer: 8
+  },
+question14: {
+    question: "90/9 = ?",
+    options: [9, 10, 11, 12],
+    answer: 10
+  },
+question15: {
+    question: "64/8 = ?",
+    options: [6, 7, 8, 9],
+    answer: 8
+  },
+question16: {
+    question: "108/9 = ?",
+    options: [10, 11, 12, 13],
+    answer: 12
+  },
+question17: {
+    question: "125/5 = ?",
+    options: [20, 22, 24, 25],
+    answer: 25
+  },
+question18: {
+    question: "40/5 = ?",
+    options: [6, 7, 8, 9],
+    answer: 8
+  },
+question19: {
+    question: "84/12 = ?",
+    options: [5, 6, 7, 8],
+    answer: 7
+  },
+question20: {
+    question: "56/8 = ?",
+    options: [5, 6, 7, 8],
+    answer: 7
+  }
+
+}
+
+const getRandomQuestions = () => {
+  
 }
